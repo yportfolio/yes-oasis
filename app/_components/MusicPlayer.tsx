a2f3ea15725d64bc02dbdf2ce1b0e6c103273d59
yewang <yewang@redhat.com>
2024-03-09 17:14:04 +0800

    fix: update music player

--- app/_components/MusicPlayer.tsx
+++ app/_components/MusicPlayer.tsx
@@ -1,11 +1,32 @@
 "use client";
-import React from "react";
+import { Play, PauseCircle } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function MusicPlayer() {
+  const [playing, setPlaying] = useState(false);
+  const ref = useRef<HTMLAudioElement>(null);
+
+  const onPlay = () => {
+    ref.current?.play();
+    setPlaying(true);
+  };
+
+  const onPause = () => {
+    ref.current?.pause();
+    setPlaying(false);
+  };
+
   return (
     <figure className="flex items-center">
-      <figcaption>Boulevard of Broken Dreams</figcaption>
-      <audio autoPlay src="/media/Boulevard-of-Broken-Dreams.mp3"></audio>
+      <figcaption className={`${playing && "animate-pulse"}`}>
+        Boulevard of Broken Dreams
+      </figcaption>
+      <audio ref={ref} src="/media/Boulevard-of-Broken-Dreams.mp3" />
+      {playing ? (
+        <PauseCircle className="cursor-pointer" onClick={onPause} />
+      ) : (
+        <Play className="cursor-pointer" onClick={onPlay} />
+      )}
     </figure>
   );
 }
